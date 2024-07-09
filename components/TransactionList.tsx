309363f8ee761b2610cf386f75e862d14bbd8e96
Eric Penrod <emp923@gmail.com>
2024-07-09 15:00:37 -0400

    TransactionList history

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,5 +1,6 @@
 import getTransactions from '@/app/actions/getTransactions';
 import { Transaction } from '@/types/Transaction';
+import TransactionItem from '@/components/TransactionItem';
 
 const TransactionList = async () => {
   const { transactions, error } = await getTransactions();
@@ -13,7 +14,7 @@ const TransactionList = async () => {
       <h3>History</h3>
       <ul className="list">
         { transactions && transactions.map((transaction: Transaction) => (
-          <p>{transaction.text}</p>
+          <TransactionItem key={transaction.id} transaction={transaction} />
         ))}
       </ul>
     </>
